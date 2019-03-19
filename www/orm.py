2e64a10f8a4255b1d05e59ec9a37ef285355cde5
嵇明新 <jimingxin@cloudwalk.cn>
2019-03-19 17:57:30 +0800

    first commit

---
+++ www/orm.py
@@ -0,0 +1,42 @@
+
+__author__ = 'MingXin.Ji'
+
+'''
+orm 
+'''
+
+import asyncio,logging
+
+import aiomysql
+
+def log(sql,args=()):
+    logging.info('SQL:%s' % sql)
+
+async def create_pool(loop,**kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host','localhost'),
+        port=kw.get('port',3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset','uft8'),
+        autocommit=kw.get('charset','utf8'),
+        maxsize=kw.get('maxsize',10),
+        minsize=kw.get('minsize',1),
+        loop=loop
+    )
+
+async def select(sql,args,size=None):
+    log(sql,args)
+    global __pool
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?','%s'),args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+        logging.info('rows returned:%s' %len(rs))
+        return rs
